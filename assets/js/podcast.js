20b8ab2b606d000fb15fa03e6cd96532f7bdd194
DanielTiozo <danieltiozo@hotmail.com>
2024-12-02 12:37:53 -0300

    começando responsividade youtube

--- assets/js/podcast.js
+++ assets/js/podcast.js
@@ -1,27 +1,66 @@
-const youtubeVideo = document.querySelector(".last-youtube-video");
+const channelId = "UCXYvwJbbIm-o1O4SthRV2Lw"; // ID do canal
+const apiKey = "AIzaSyBqX7F7b9ReeIZV2WKElLz9fMvvikeZtOM"; // Substitua pela sua chave da API
+const videoList = document.getElementById("video-list");
+const videoContainer = document.getElementById("video-container");
 
-async function searcLastYoutubeVideo() {
-	youtubeVideo.innerHTML = "";
-	const lastVideo = await fetch(
-		"https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUXYvwJbbIm-o1O4SthRV2Lw&key=AIzaSyBA11F7Oqv_9sUxmDcwXi3facdB08IKRNw",
-	)
-		.then((res) => res.json())
+// Função para buscar os vídeos mais recentes
+function fetchRecentVideos(pageToken = "") {
+	const searchUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=9&order=date&type=video&pageToken=${pageToken}&key=${apiKey}`;
+
+	fetch(searchUrl)
+		.then((response) => response.json())
 		.then((data) => {
-			youtubeVideo.innerHTML += `
-			<a target='_blank' href='https://www.youtube.com/watch?v=${data.items[0].snippet.resourceId.videoId}' >
-				<div class='youtube-container'>	
-					<p class='youtube-title'>${data.items[0].snippet.title}</p>
-					<img class='youtube-img' src='${data.items[0].snippet.thumbnails.maxres.url}'>
-				<div>
-			</a>
-			`;
+			const nextPageToken = data.nextPageToken;
+
+			// Adiciona vídeos à UI
+			// biome-ignore lint/complexity/noForEach: <explanation>
+			data.items.forEach((video) => {
+				const videoCard = document.createElement("div");
+				videoCard.classList.add("video-card");
+
+				const videoThumbnail = video.snippet.thumbnails.medium.url;
+				const videoTitle = video.snippet.title;
+				const videoLink = `https://www.youtube.com/watch?v=${video.id.videoId}`;
+
+				videoCard.innerHTML = `
+                    <a class='link-video' href="${videoLink}" target="_blank">
+                        <p>${videoTitle}</p>
+                        <img src="${videoThumbnail}" alt="${videoTitle}">
+                    </a>
+                `;
+
+				videoList.appendChild(videoCard);
+			});
+
+			// Se existir mais páginas, configura o scroll infinito
+			if (nextPageToken) {
+				window.onscroll = () => {
+					if (
+						window.innerHeight + window.scrollY >=
+						document.documentElement.scrollHeight - 50
+					) {
+						fetchRecentVideos(nextPageToken);
+						window.onscroll = null; // Evita múltiplas requisições ao rolar
+					}
+				};
+			}
+
+			const linkVideo = document.querySelector(".link-video");
+			const body = document.getElementsByTagName("body");
+
+			videoList.style.textDecoration = "none";
+			videoList.style.textAlign = "center";
+			videoList.style.display = "grid";
+			// const tela = body.style.width;
+			// if (tela > 400) {
+			videoList.style.gridTemplateColumns = "repeat(3, 1fr)";
+			// }
+			// videoList.style.margin = "20px";
+			// videoList.style.padding = "20px";
+			// videoContainer.style.margin = "20px";
 		})
-		.catch((err) => {
-			console.log(err);
-			youtubeVideo.innerHTML +=
-				"<h5>Algo não saiu como esperado, tente novamente mais tarde!</h5>";
-		});
+		.catch((error) => console.error("Erro ao buscar vídeos:", error));
 }
 
-setInterval(searcLastYoutubeVideo, 60000);
-searcLastYoutubeVideo();
+// Inicializa a busca com os vídeos mais recentes
+fetchRecentVideos();
